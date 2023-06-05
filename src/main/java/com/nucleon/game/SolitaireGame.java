7ad52a2df4e1858d9e1acdecf147722e0003ec4f
MaoJiayang <nucleon_17th@njust.edu.cn>
2023-06-05 16:27:37 +0800

    202306051627:更新了提示功能,每一回合需要提示的时候三次应该会给出不同的结果

--- src/main/java/com/nucleon/game/SolitaireGame.java
+++ src/main/java/com/nucleon/game/SolitaireGame.java
@@ -52,12 +52,12 @@ public class SolitaireGame extends GameFlow {
         }
         //2.判断该成语是否合法
         while (!RefereeSystem.isValidIdiom(idiom)) {
-            System.out.println("您输入的成语被使用过或不存在，请重新输入：");
+            System.out.println("您输入的成语不存在，请重新输入：");
             idiom = sc.nextLine();
             if ("exit".equals(idiom)) {
-            System.out.println("游戏结束");
-            return;
-        }
+                System.out.println("游戏结束");
+                return;
+            }
         }
         //3.将用户输入成语加入已使用成语列表
         RefereeSystem.updateUsedIdioms(idiom);
@@ -67,6 +67,32 @@ public class SolitaireGame extends GameFlow {
         System.out.println("电脑回合：" + computerIdiom.getWord());
         //5.接受用户的接龙成语,如果用户选择需要提示,则给用户提供一个可接龙的成语作为提示.重复2-7步骤,直到用户输入exit
         while (true) {
+            System.out.println("输入exit退出，输入hint提示.剩余提示次数" + RefereeSystem.getAvailableHintCount() + "\n请输入一个成语:");
+            String userAnswer = sc.nextLine();
+            if ("exit".equals(userAnswer)) {
+                System.out.println("游戏结束，您的得分为："+ RefereeSystem.getCurrentScore());
+                break;
+            }
+            if ("hint".equals(userAnswer) && RefereeSystem.getAvailableHintCount() > 0) {
+                String hintIdiom = RefereeSystem.doOneRound(computerIdiom.getWord()).getWord();
+                System.out.println("提示：" + hintIdiom);
+                RefereeSystem.setAvailableHintCount(RefereeSystem.getAvailableHintCount() - 1);
+                RefereeSystem.updateUsedIdioms(hintIdiom);
+                System.out.println("剩余提示次数:" + RefereeSystem.getAvailableHintCount());
+                continue;
+            }
+            //TODO:复用提示功能.应该重写一个提示方法
+            //6.判断该成语是否合法
+            if (!RefereeSystem.isValidIdiom(userAnswer)) {
+                System.out.println("您输入的成语被使用过或不存在，请重新输入");
+                continue;//回到循环头,重新输入
+            }
+            //8.将用户输入成语加入已使用成语列表
+            RefereeSystem.updateUsedIdioms(userAnswer);
+            //9.查找符合难度要求的,未被使用过的成语作为电脑回合的成语,展示该成语,并加入已使用成语列表.
+            computerIdiom = RefereeSystem.doOneRound(userAnswer);
+            RefereeSystem.updateUsedIdioms(computerIdiom.getWord());
+            System.out.println("电脑回合：" + computerIdiom.getWord());
             //难度自减100
             int currentDifficulty = RefereeSystem.getCurrentDifficulty();
             if (currentDifficulty < 100){
@@ -75,35 +101,6 @@ public class SolitaireGame extends GameFlow {
             else{
                 RefereeSystem.setCurrentDifficulty(currentDifficulty - 100);
             }
-            
-            System.out.println("输入exit退出，输入hint提示.剩余提示次数" + RefereeSystem.getAvailableHintCount() + "\n请输入一个成语:");
-            String userAnswer = sc.nextLine();
-            if ("exit".equals(userAnswer)) {
-                System.out.println("游戏结束，您的得分为："+ RefereeSystem.getCurrentScore());
-                break;
-            }
-            if ("hint".equals(userAnswer) && RefereeSystem.getAvailableHintCount() > 0) {
-                System.out.println("提示：" + RefereeSystem.doOneRound(computerIdiom.getWord()).getWord());
-                RefereeSystem.setAvailableHintCount(RefereeSystem.getAvailableHintCount() - 1);
-                System.out.println("剩余提示次数:" + RefereeSystem.getAvailableHintCount());
-                continue;
-            }
-            //TODO:复用提示功能.应该重写一个提示方法
-            //6.判断该成语是否合法
-            while (!RefereeSystem.isValidIdiom(userAnswer)) {
-                System.out.println("您输入的成语被使用过或不存在，请重新输入：");
-                userAnswer = sc.nextLine();
-                if ("exit".equals(idiom)) {
-                System.out.println("游戏结束，您的得分为："+ RefereeSystem.getCurrentScore());
-                return;
-                }
-            }
-            //8.将用户输入成语加入已使用成语列表
-            RefereeSystem.updateUsedIdioms(userAnswer);
-            //9.查找符合难度要求的,未被使用过的成语作为电脑回合的成语,展示该成语,并加入已使用成语列表.
-            computerIdiom = RefereeSystem.doOneRound(userAnswer);
-            RefereeSystem.updateUsedIdioms(computerIdiom.getWord());
-            System.out.println("电脑回合：" + computerIdiom.getWord());
         }    
     }
 
